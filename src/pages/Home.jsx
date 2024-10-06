6201f8f10b0215e1faa5c277e19a33c1f15ea2fe
threelokin <threelok.in@gmail.com>
2024-10-06 16:31:34 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import NewsList from '../components/NewsList';
+import ReactGA from "react-ga4"
 
 const Home = ({ language }) => {
   const handleScroll = (scrollTop) => {
     // Handle scroll logic here if needed
   };
+
+  ReactGA.send({
+    hitType: "pageview",
+    page: "/",
+    title: "Home",
+    });
 
   return (
     <div className="h-screen bg-gray-300">
